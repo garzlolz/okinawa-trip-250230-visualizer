e24d6b54a3ef22c106c019768737852564a1a256
mac-finn-shih <oscar861213gmail.com>
2026-04-02 21:12:44 +0800

    fix: 修改文本內容以增強個人情感表達

--- js/views/App.js
+++ js/views/App.js
@@ -145,7 +145,7 @@ export default {
               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
-              給她的一段話
+              給我和她的一段話
             </h2>
             <div class="text-gray-600 leading-relaxed text-sm md:text-base space-y-3 relative z-10">
 <p>
